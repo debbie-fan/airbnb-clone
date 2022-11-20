335727a03a870bad875f48e444effde97b29a953
Debbie Fan <debbiehmfan@outlook.com>
2022-11-20 17:15:51 -0500

    Add experience status on top left of each card to show 'sold out' or 'online' if applicable

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,17 +1,26 @@
 function Card(props) {
-    return (
-      <section className='card'>
-        <img src={require('../assets/' + props.img)} alt='Katie Zaferes' className='card-img'/>
-        <div className='card-stats'>
-          <img src={require('../assets/star.png')} alt='star' className='card-star'/>
-          <span>{props.rating}</span>
-          <span className='gray'>({props.reviewCount}) • </span>
-          <span className='gray'>{props.country}</span>
-        </div>
-        <p>{props.title}</p>
-        <p><span className='bold'>From ${props.price}</span> / person</p>
-      </section>
-    );
+
+  let badgeText
+  if (props.openSpots === 0) {
+    badgeText = 'SOLD OUT'
+  } else if (props.country === 'Online') {
+    badgeText = 'ONLINE'
+  }
+
+  return (
+    <div className='card'>
+      {badgeText && <div className='card-badge'>{badgeText}</div>}
+      <img src={require('../assets/' + props.img)} alt='experience topic' className='card-img'/>
+      <div className='card-stats'>
+        <img src={require('../assets/star.png')} alt='star' className='card-star'/>
+        <span>{props.rating}</span>
+        <span className='gray'>({props.reviewCount}) • </span>
+        <span className='gray'>{props.country}</span>
+      </div>
+      <p>{props.title}</p>
+      <p><span className='bold'>From ${props.price}</span> / person</p>
+    </div>
+  );
 }
 
 export default Card;
