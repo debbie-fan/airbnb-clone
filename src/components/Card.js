d30ec14f1185c3f8004844618d9458a3f61425fb
Debbie Fan <debbiehmfan@outlook.com>
2022-11-20 23:54:07 -0500

    Spread object as props in Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,24 +1,24 @@
 function Card(props) {
 
   let badgeText
-  if (props.item.openSpots === 0) {
+  if (props.openSpots === 0) {
     badgeText = 'SOLD OUT'
-  } else if (props.item.location === 'Online') {
+  } else if (props.location === 'Online') {
     badgeText = 'ONLINE'
   }
 
   return (
     <div className='card'>
       {badgeText && <div className='card-badge'>{badgeText}</div>}
-      <img src={require('../assets/' + props.item.coverImg)} alt='experience topic' className='card-img'/>
+      <img src={require('../assets/' + props.coverImg)} alt='experience topic' className='card-img'/>
       <div className='card-stats'>
         <img src={require('../assets/star.png')} alt='star' className='card-star'/>
-        <span>{props.item.stats.rating}</span>
-        <span className='gray'>({props.item.stats.reviewCount}) • </span>
-        <span className='gray'>{props.item.location}</span>
+        <span>{props.stats.rating}</span>
+        <span className='gray'>({props.stats.reviewCount}) • </span>
+        <span className='gray'>{props.location}</span>
       </div>
-      <p>{props.item.title}</p>
-      <p><span className='bold'>From ${props.item.price}</span> / person</p>
+      <p>{props.title}</p>
+      <p><span className='bold'>From ${props.price}</span> / person</p>
     </div>
   );
 }
